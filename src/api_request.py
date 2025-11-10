1f782ffbe68489ecb7c09118f0b821c356452a4f
Urffin-Juss <mellondefall@gmail.com>
2025-11-10 11:21:21 +0300

    get api func

--- src/api_request.py
+++ src/api_request.py
@@ -1,3 +1,34 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Any, Dict, List
 import requests
+import HH_API_URL
+
+class BaseAPI(ABC):
+
+    @abstractmethod
+    def get_vacancies(self, *arg, **kwargs) -> list[Dict[[str, Any]]:
+        pass
+
+
+class HH_API(BaseAPI):
+        BASE_URL = f"{HH_API_URL}/vacancies"
+
+
+        def get_vacancies(
+                self,
+                text: str,
+                page: int = 0,
+                per_page: int = 50,
+                area: int = 123,
+        ): -> list[Dict[str, Any]]:
+                params = {
+                    "text" : text,
+                    "page" : page,
+                    "per_page" : per_page,
+                    "area" : area,
+                }
+                resp = requests.get(self.BASE_URL, params=params, timeout=10)
+                resp.raise_for_status()
+                data = resp.json()
+                return data.get("items", [])
+
